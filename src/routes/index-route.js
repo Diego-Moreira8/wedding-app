8d43ceec579c8103ff4420dbb9de4ae850d7d8cb
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2025-03-25 21:56:39 -0300

    Updated index

--- src/routes/index-route.js
+++ src/routes/index-route.js
@@ -1,9 +1,18 @@
 const { Router } = require("express");
+const prisma = require("../prisma/client");
 
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res, next) => {
+indexRouter.get("/", async (req, res, next) => {
   if (req.user) {
+    const giftChosen = await prisma.gift.findUnique({
+      where: { userId: req.user.id },
+    });
+
+    if (giftChosen) {
+      return res.redirect("/presentes/minha-escolha");
+    }
+
     return res.redirect("/presentes");
   }
 
