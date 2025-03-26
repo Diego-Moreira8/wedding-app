5303c995dedb14ba9e90709d5d12b6bb799a516b
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2025-03-26 20:17:20 -0300

    Login styles

--- src/app.js
+++ src/app.js
@@ -15,9 +15,11 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "../public")));
+console.log(path.join(__dirname, "../public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(expressSession(sessionOptions));
+app.use(logger("dev"));
 app.use(passport.session());
 app.use(addUserToLocals);
 
